6aed6b9b73f70b843ed7cd985f2622c4ef583a76
pk karki <prabin@abhiyantrik.com>
2020-07-11 21:49:28 +0545

    image compress

--- js/custom.js
+++ js/custom.js
@@ -146,35 +146,6 @@
 
    });
 
-//=====================================================================================
-//  06.   Testimonial Slider
-//=====================================================================================
-
-    $('.testimonial_item').owlCarousel({
-     loop: true,
-     autoplay: true,
-     autoplayTimeout: 5000,
-     margin: 0,
-     nav: true,
-     dots: false,
-     navText: ['<span class="fa fa-angle-left"></span>','<span class="fa fa-angle-right"></span>'],
-     responsive:{
-
-        0:{
-          items:1
-        },
-        600:{
-          items:1
-        },
-        1024:{
-          items:2
-        },
-        1200:{
-          items:2
-        }
-      }
-
-     });
 
 //=====================================================================================
 // 07.    Fact Counter
